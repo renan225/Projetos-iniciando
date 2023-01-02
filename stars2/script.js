7115da0ebc38f09cae6b912e558776bfa179cc04
Renan Oliveira de Souza <114809416+renan225@users.noreply.github.com>
2023-01-02 09:23:23 -0300

    mudei fotos

--- stars2/script.js
+++ stars2/script.js
@@ -5,7 +5,7 @@ function menuShow() {
         document.querySelector('.icon').src = "img/menu_white_36dp.svg"
         document.querySelector('.principal').style.cssText = 'margin-top: 0px;'
     } else {
-        document.querySelector('.principal').style.cssText = 'margin-top: 40%;'
+        document.querySelector('.principal').style.cssText = 'margin-top: 50%;'
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "img/close_white_36dp.svg"
     }
